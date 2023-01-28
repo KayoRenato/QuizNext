cb0a5d5f8f4f355ee29a1d09c52765bae4b3cb12
Kayo.io <kayorenatocontato@gmail.com>
2023-01-28 15:34:18 -0300

    Chore: Add Animation on Flip Answer

--- components/Answers.tsx
+++ components/Answers.tsx
@@ -12,40 +12,40 @@ interface AnswerProps {
 export default function Answer(props: AnswerProps) {
 
     const answer = props.value
+    const answerUncovered = answer.uncovered ? styles.answerUncovered : ''
 
     return (
         <div className={styles.answer}
             onClick={() => props.onResponse(props.idx)}>
-            <div className={styles.contentAnswer}>
-                {!answer.uncovered ? (
-                    <div className={styles.frontFlipper}>
-                        <div className={styles.letter}
-                            style={{ backgroundColor: props.letterBackgroundColor }}>
-                            {props.letter}
-                        </div>
-                        <div className={styles.value}>
-                            {answer.value}
-                        </div>
+            <div className={`${styles.contentAnswer} ${answerUncovered}`}>
+
+                <div className={styles.frontFlipper}>
+                    <div className={styles.letter}
+                        style={{ backgroundColor: props.letterBackgroundColor }}>
+                        {props.letter}
                     </div>
-                ) : (
-                    <div className={styles.backFlipper}>
-                        {answer.correct ? (
-                            <div className={styles.correct}>
-                                <div>
-                                    The answer right is ...
-                                </div>
-                                <div className={styles.value}>{answer.value}</div>
-                            </div>
-                        ) : (
-                            <div className={styles.incorrect}>
-                                <div>
-                                    The answer sent is wrong.
-                                </div>
-                                <div className={styles.value}>{answer.value}</div>
-                            </div>
-                        )}
+                    <div className={styles.value}>
+                        {answer.value}
                     </div>
-                )}
+                </div>
+                
+                <div className={styles.backFlipper}>
+                    {answer.correct ? (
+                        <div className={styles.correct}>
+                            <div>
+                                The answer right is ...
+                            </div>
+                            <div className={styles.value}>{answer.value}</div>
+                        </div>
+                    ) : (
+                        <div className={styles.incorrect}>
+                            <div>
+                                The answer sent is wrong.
+                            </div>
+                            <div className={styles.value}>{answer.value}</div>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     )
